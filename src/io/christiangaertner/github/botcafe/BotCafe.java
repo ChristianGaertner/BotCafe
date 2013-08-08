46bd3203be827ef3dd6625db4251e554837dc229
ChristianGaertner <christiangaertner.film@googlemail.com>
2013-08-08 19:56:38 +0200

    Initial Commit

---
+++ src/io/christiangaertner/github/botcafe/BotCafe.java
@@ -0,0 +1,18 @@
+package io.christiangaertner.github.botcafe;
+
+/**
+ *
+ * @author Christian
+ */
+public class BotCafe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BotCafe cafe = new BotCafe();
+    }
+
+    public BotCafe() {
+    }
+}
